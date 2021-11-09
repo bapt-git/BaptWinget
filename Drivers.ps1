6ccf27a0e6e7ac2e7b31145aafa10c7302eb98ae
bapt-git <93990351+bapt-git@users.noreply.github.com>
2021-11-09 15:07:47 +0100

    Create Drivers.ps1

---
+++ Drivers.ps1
@@ -0,0 +1,22 @@
+$proc = Get-WmiObject -Class Win32_Processor | Select-Object -Property Name
+
+Write-Host "Installing CPU driver assistant"
+if ($proc -match "Intel") {
+    winget install --id Intel.IntelDriverAndSupportAssistant -e
+}
+elseif ($proc -match "AMD") {
+    Write-host "Driver assistant not found"
+}
+else {
+    Write-Host "Driver assistant for CPU not installed"
+}
+$gpus = Get-WmiObject -Class Win32_VideoController | Select-Object -Property Description
+Write-Host "Installing GPU driver assistant"
+foreach ($gpu in $gpus) {
+    if ($gpu -match "NVIDIA") {
+        winget install --id Nvidia.GeForceExperience -e 
+    }
+    elseif ($gpu -match "AMD") {
+        Write-host "Driver for AMD GPUs not found"
+    }
+}
