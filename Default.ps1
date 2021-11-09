44f4f4d4ddcca34ae120c9da083fb2f1446f3d1a
bapt-git <93990351+bapt-git@users.noreply.github.com>
2021-11-09 15:11:55 +0100

    Update Default.ps1

--- Default.ps1
+++ Default.ps1
@@ -1,6 +1,3 @@
-#Drivers script
-.\Drivers.ps1
-
 #Games script
 .\Games\Games.ps1
 
@@ -15,3 +12,12 @@
 
 #Uninstal shit
 .\Uninstall\Uninstal.ps1
+
+$Drivers = "0"
+
+while ($Drivers -ne "y" -and $Drivers -ne "n") {
+    $Drivers = Read-Host "Installation Drivers CPU et GPU ? (y/n) "
+    if ($Drivers -eq "y") {
+        .\Drivers.ps1
+    }
+}
