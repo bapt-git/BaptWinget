f48bbc5e55259fce4af357692b44eaf580bc072c
bapt-git <93990351+bapt-git@users.noreply.github.com>
2021-11-09 15:06:19 +0100

    Update Utils.ps1

--- Utils/Utils.ps1
+++ Utils/Utils.ps1
@@ -1,7 +1,7 @@
 $utils = "0"
 
 while ($utils -ne "y" -and $utils -ne "n") {
-    $utils = Read-Host "Install utils stuff? (y/n) "
+    $utils = Read-Host "Installation des logiciels indispensables à tout PC? (y/n) "
     if ($utils -eq "y") {
         foreach ($line in Get-Content $PSSCriptRoot\utils.txt) {
             if ($line.contains("#")) {
