1488bfe72960ff36138c492bfedf71c740a2f23d
bapt-git <93990351+bapt-git@users.noreply.github.com>
2021-11-09 15:04:33 +0100

    Update games.ps1

--- Games/games.ps1
+++ Games/games.ps1
@@ -1,7 +1,7 @@
 $games = "0"
 
 while ($games -ne "y" -and $games -ne "n") {
-    $games = Read-Host "Install gaming stuff? (y/n) "
+    $games = Read-Host "Installer les logiciels par défaut de gaming ? (y/n) "
     if ($games -eq "y") {
         foreach ($line in Get-Content $PSSCriptRoot\games.txt) {
             if ($line.contains("#")) {
