9b3ac3b8ed03bb17f2bb05de14d96c0a8d4d9bc4
bapt-git <93990351+bapt-git@users.noreply.github.com>
2021-11-22 16:30:52 +0100

    Update Default.ps1

--- Default.ps1
+++ Default.ps1
@@ -18,6 +18,6 @@ $Drivers = "0"
 while ($Drivers -ne "y" -and $Drivers -ne "n") {
     $Drivers = Read-Host "Installation Drivers CPU et GPU ? (y/n) "
     if ($Drivers -eq "y") {
-        .\Drivers.ps1
+        $PSScriptRoot\Drivers.ps1
     }
 }
